e0dd27300402e1daa41bcd8d73b589407986f624
nicomesa20 <nicomesa2013@gmail.com>
2022-12-21 12:05:21 -0500

    fixing api prefix

--- src/features/companies/companiesService.ts
+++ src/features/companies/companiesService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = `http://${import.meta.env.VITE_API_URL}/company`
+const API_URL = `${import.meta.env.VITE_API_URL}/company`
 
 const companyAPI = axios.create({ baseURL: API_URL })
 
--- src/features/product/productService.ts
+++ src/features/product/productService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = `http://${import.meta.env.VITE_API_URL}/article`
+const API_URL = `${import.meta.env.VITE_API_URL}/article`
 
 const productAPI = axios.create({ baseURL: API_URL })
 
